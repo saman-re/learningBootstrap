64b3e1f50f837fb904435c5a6e682582de39560f
saman-re <saman.rezapoor1379@yahoo.com>
2021-03-24 01:07:34 +0430

    project(learning bootstrap) almost done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <button class="btn btn-primary">hello</button>
+    <NavBar />
+    <div class="row mx-0">
+      <div class="col-12 col-md-6 order-2 order-md-1 px-0 py-2">
+        <img src="./assets/Gobin.jpeg" alt="Gobin" width="100%">
+      </div>
+      <div class="col-12 col-md-6 order-1 order-md-2">
+      <Form/>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar";
+import Form from "./components/Form";
+export default {
+  components: { NavBar, Form },
+};
+</script>
