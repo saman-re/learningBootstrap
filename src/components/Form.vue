081d5324baf2c6060a3cfa8955ddf8f44ce42a70
saman-re <saman.rezapoor1379@yahoo.com>
2021-03-24 01:06:31 +0430

    sign-in form completed

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        Form Field
+  <div class="pt-5 mt-3">
+    <h2 class="text-center text-md-right mr-md-4">شتاب بده</h2>
+    <p class="text-center text-md-right mr-md-4">
+      با گوبین روی بیش از ۳۰ بلتفرم در آن واحد
+    </p>
+    <div dir="rtl" class="form-group text-right pt-4 px-4">
+      <label for="email">ایمیل:</label>
+      <input
+        class="form-control"
+        type="email"
+        autocomplete="off"
+        placeholder="ایمیل خود را وارد کنید"
+        id="email"
+      />
     </div>
+    <div dir="rtl" class="form-group text-right pt-1 px-4">
+      <label for="email">پسورد:</label>
+      <input
+        class="form-control"
+        type="email"
+        autocomplete="off"
+        placeholder="پسورد خود را وارد کنید"
+        id="password"
+      />
+    </div>
+    <div class="row pt-4">
+      <div class="col-6 text-right">
+        <p class="font-italic text-muted pl-1">
+          ثبت نام شما به این معنی است که با 
+          <a href="#" class="text-reset">
+          قوانین و مقرارات
+          </a>
+           ما موافقت می کنید
+        </p>
+      </div>
+      <div class="col-6 pr-5">
+        <button class="btn btn-primary py-3 w-100 font-weight-bold">
+          ثبت نام رایگان
+        </button>
+      </div>
+    </div>
+      <p class="text-center text-md-right mr-md-4 my-3">
+      هم اکنون حساب دارید؟
+      <a href="#" class="text-reset font-weight-bold">
+          ورود
+      </a>
+      </p>
+  </div>
 </template>
